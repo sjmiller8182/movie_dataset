f87a1ef34cca6df4e633e266b82251c8ffdc1a58
Stuart Miller <stuart.j.miller.8182@gmail.com>
2018-11-03 23:46:55 -0500

    add bar, scatter, and helper for linear regression plotting

--- visuals.py
+++ visuals.py
@@ -1,4 +1,6 @@
 import matplotlib.pyplot as plt
+from scipy.stats import linregress as linreg
+import numpy as np
 
 def time_plot(x, y, label, title = None, x_lab = None, y_lab = None, fig_size = (15,7.5)):
     '''
@@ -7,8 +9,6 @@ def time_plot(x, y, label, title = None, x_lab = None, y_lab = None, fig_size = 
     group_by - string
     plot_col - string or list of strings
     x_lab, y_lab, title - string
-    ag_function - list of strings (mean, median, or count)
-    
     '''
     plt.figure(figsize=(fig_size))
     
@@ -19,6 +19,61 @@ def time_plot(x, y, label, title = None, x_lab = None, y_lab = None, fig_size = 
     for i in range(len(label)):
         plt.plot(x, y[i], label = label[i]);
     plt.legend();
+    if title is not None:
+        plt.title(title);
+    if y_lab is not None:
+        plt.ylabel(y_lab);
+    if x_lab is not None:
+        plt.xlabel(x_lab);
+    plt.show();
+    
+def bar(pos, height, labels = None, title = None, x_lab = None, y_lab = None, fig_size = (15,7.5)):
+    '''
+    Makes a bar chart from positions (pos) and heights. Use labels to replace pos elements.
+    pos - positions of bars in chart
+    height - heights of bars
+    labels - labels for position vector
+    x_lab, y_lab, title - string
+    '''
+    plt.figure(figsize=(fig_size));
+    plt.bar(pos, height);
+    if labels is not None:
+        plt.xticks(pos,labels);
+    if title is not None:
+        plt.title(title);
+    if y_lab is not None:
+        plt.ylabel(y_lab);
+    if x_lab is not None:
+        plt.xlabel(x_lab);
+    plt.show();
+    
+def _make_reglin_labels(r2, pV, stdE, parms):
+    label = list()
+    params = ['r2: ','p-V: ', 'stdE: ']
+    r2 = np.round(r2, 2)
+    pv = np.round(pV, 2)
+    stdE = np.round(stdE, 2)
+    for p in parms:
+        if p == 'r2':
+            label.append(params[0] + str(r2))
+        elif p == 'pV':
+            label.append(params[1] + str(pv))
+        elif p == 'stdE':
+            label.append(params[2] + str(stdE))
+        else:
+            pass
+    return '\n'.join(label)
+    
+def scatter(x, y, label = None, title = None, x_lab = None, y_lab = None, add_trend_line = False, line_color = 'r', line_char = None, fig_size = (15,7.5)):
+    '''
+    Makes a bar chart from positions (pos) and heights. Use labels to replace pos elements.
+    '''
+    plt.figure(figsize=(fig_size));
+    
+    plt.scatter(x, y, label);
+    if add_trend_line:
+        m, b, r2, pv, std_error = linreg(x,y)
+        plt.plot(x, m * x + b,'-', c = line_color, label = _make_reglin_labels(r2, pv, std_error))
     if title is not None:
         plt.title(title);
     if y_lab is not None:
